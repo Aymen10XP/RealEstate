4b6821b4257f8bc0d536bc1a0b10fcf1b5f220fc
Aymen10XP <125681512+Aymen10XP@users.noreply.github.com>
2025-10-15 13:49:31 +0100

    Added Controllers for authentication and dashboard

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -1,1 +1,67 @@
 <?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AuthenticationController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('authentication/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            // Default role for new users
+            $user->setRoles(['ROLE_TENANT']);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('authentication/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,1 +1,60 @@
 <?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
+            return $this->redirectToRoute('admin_dashboard');
+        } elseif (in_array('ROLE_MANAGER', $user->getRoles())) {
+            return $this->redirectToRoute('manager_dashboard');
+        } elseif (in_array('ROLE_OWNER', $user->getRoles())) {
+            return $this->redirectToRoute('owner_dashboard');
+        } elseif (in_array('ROLE_TENANT', $user->getRoles())) {
+            return $this->redirectToRoute('tenant_dashboard');
+        }
+
+        return $this->redirectToRoute('app_login');
+    }
+
+    #[Route('/admin/dashboard', name: 'admin_dashboard')]
+    public function adminDashboard(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        return $this->render('dashboard/admin.html.twig');
+    }
+
+    #[Route('/manager/dashboard', name: 'manager_dashboard')]
+    public function managerDashboard(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_MANAGER');
+
+        return $this->render('dashboard/manager.html.twig');
+    }
+
+    #[Route('/owner/dashboard', name: 'owner_dashboard')]
+    public function ownerDashboard(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_OWNER');
+
+        return $this->render('dashboard/owner.html.twig');
+    }
+
+    #[Route('/tenant/dashboard', name: 'tenant_dashboard')]
+    public function tenantDashboard(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_TENANT');
+
+        return $this->render('dashboard/tenant.html.twig');
+    }
+}
